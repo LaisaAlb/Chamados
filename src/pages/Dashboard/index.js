31c5b14bb98b4906908565353f45f16d0c74d179
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-03-30 12:07:05 -0300

    Estilizando a Tabela

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,11 @@ import { useContext } from 'react'
 import { AuthContext } from '../../contexts/auth'
 
 import Header from '../../components/Header/index'
+import Title from '../../components/Title'
+import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from 'react-icons/fi'
+
+import { Link } from 'react-router-dom'
+import './dashboard.css'
 
 export default function Dashboard(){
     const { logout } = useContext(AuthContext);
@@ -13,7 +18,53 @@ export default function Dashboard(){
     return (
         <div>
             <Header />
-            <h1>Página de deshboard</h1>
+
+            <div className='content'>
+                <Title name="Chamados">
+                    <FiMessageSquare size={25}/>
+                </Title>
+
+                <>
+                    <Link className="new" to="/new">
+                        <FiPlus color="#FFF" size={25}/>
+                        Novo Chamado
+                    </Link>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Cadastrado em</th>
+                                <th scope="col">Ações</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td data-label="Cliente"> Mercado Esquina </td>
+                                <td data-label="Assunto"> Suporte </td>
+                                <td data-label="Status"> 
+                                    <span className='badge' style={{ backgroundColor: '#999' }}>
+                                        Em aberto
+                                    </span>
+                                </td>
+                                <td data-label="Cadastrado"> 10/10/2005 </td>
+                                <td data-label="#"> 
+                                    <button className='action' style={{backgroundColor: '#3583f6'}}>  
+                                        <FiSearch color="#FFF" size={17}/>
+                                    </button>
+                                    <button className='action' style={{backgroundColor: '#f6a935'}}> 
+                                        <FiEdit2 color="#FFF" size={17}/>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </>
+
+            </div>
             <button onClick={handleLogout}>Sair da conta</button>
         </div>
     )
